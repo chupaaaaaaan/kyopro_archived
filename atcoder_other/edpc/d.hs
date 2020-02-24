1facccc0c354d5dbd97a416ca8b8021e6cf5b9b7
Takayuki Uchida <jacobitebeerlove@gmail.com>
2020-02-24 17:35:01 +0900

    solved.

--- atcoder_other/edpc/d.hs
+++ atcoder_other/edpc/d.hs
@@ -12,14 +12,6 @@ getAsInt =  unfoldr (BS.readInt . BS.dropWhile isSpace) <$> BS.getLine
 getAsIntLine :: Int -> IO [[Int]]
 getAsIntLine n = replicateM n getAsInt
 
-getAsIntArray1Dr :: Int -> IO (UArray Int Int)
-getAsIntArray1Dr n = U.listArray (1,n) <$> getAsInt
-
-getAsIntArray1Dc :: Int -> IO (UArray Int Int)
-getAsIntArray1Dc n = U.listArray (1,n) . concat <$> getAsIntLine n
-
--- n: number of lines
--- m: number of values per a line
 getAsIntArray2D :: Int -> Int -> IO (UArray (Int,Int) Int)
 getAsIntArray2D n m = U.listArray ((1,1),(n,m)) . concat <$> getAsIntLine n
 
@@ -28,11 +20,20 @@ main :: IO ()
 main = do
   [n,w] <- getAsInt
   wvs <- getAsIntArray2D n 2
-  print wvs
+  let solve = solve1
+      result = solve wvs n w
+  print $ maximum $ map (\x -> result U.! (n,x)) [0..w]
 
 solve1 :: UArray (Int,Int) Int -> Int -> Int -> UArray (Int,Int) Int
 solve1 wvs n' w' = runSTUArray $ do
-  dp <- newArray ((1,1),(n',w')) 0
-  -- forM_ [(n_,w_) | n_ <- [1..n'], w_ <- [1..w']] $ \(n,w) -> do
-  --   writeArray dp (x,w') 1
+  dp <- newArray ((0,0),(n',w')) 0
+  forM_ [(n_,w_) | n_ <- [1..n'], w_ <- [0..w']] $ \(n,w) -> do
+    let wtmp = wvs U.! (n,1)
+        vtmp = wvs U.! (n,2)
+    if w - wtmp < 0
+      then readArray dp (n-1,w) >>= writeArray dp (n,w)
+      else do
+        a <- readArray dp (n-1,w)
+        b <- readArray dp (n-1,w-wtmp)
+        writeArray dp (n,w) $ max a (b+vtmp)
   return dp
